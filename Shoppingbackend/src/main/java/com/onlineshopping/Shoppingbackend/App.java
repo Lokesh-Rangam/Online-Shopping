a73860a0c2fe700e1cd6bf8adc4565df52445681
lokeshrangam <lokeshrangam53@gmail.com>
2019-10-03 19:47:52 +0530

    My project is configured successfully

---
+++ Shoppingbackend/src/main/java/com/onlineshopping/Shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.onlineshopping.Shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
